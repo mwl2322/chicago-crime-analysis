b21bd4907a0fdb391a1f633c712a96ca4243fdba
MOHAMAD LOULOU <80181982+mwl2322@users.noreply.github.com>
2026-01-11 02:08:26 +0300

    Update 02_load_data.sql

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,7 +1,11 @@
 -- sql/02_load_data.sql
 -- Load Chicago crime subset data into PostgreSQL
+-- Tip:
+--   - Use \copy (psql client-side) for local files (most portable).
+--   - If you are running this on the DB server and the file is accessible to the server user, you can use COPY instead.
 
-COPY chicago.crime_subset (
+-- Option A (recommended): psql client-side load
+\copy chicago.crime_subset (
     crime_id,
     case_number,
     crime_date,
@@ -15,6 +19,13 @@ COPY chicago.crime_subset (
     latitude,
     longitude
 )
-FROM '/path/to/chicago_crime_subset.csv'
-DELIMITER ','
-CSV HEADER;
+FROM 'data/chicago_crime_subset.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
+
+-- Option B (server-side): only if the CSV is on the database server
+-- COPY chicago.crime_subset (
+--     crime_id, case_number, crime_date, primary_type, description,
+--     location_description, arrest, domestic, community_area, year, latitude, longitude
+-- )
+-- FROM '/absolute/server/path/chicago_crime_subset.csv'
+-- WITH (FORMAT csv, HEADER true, DELIMITER ',', NULL '');
